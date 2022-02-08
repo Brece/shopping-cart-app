523e13cce65d01107d7f99a148aa39471395394b
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-02-08 07:06:33 +0100

    add eventlistener for to-top-btn

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { Outlet } from 'react-router-dom';
@@ -6,12 +6,33 @@ import Header from './Header';
 import Footer from './Footer';
 
 const Layout = () => {
+	const handleScroll = () => {
+		const scrollToTopBtn = document.querySelector('.c-btn-top');
+		let scrollTotal = window.scrollY;
+
+		if (scrollTotal > 200) {
+			scrollToTopBtn.classList.add('is-active');
+		} else {
+			scrollToTopBtn.classList.remove('is-active');
+		}
+	}
+
+	const handleScrollToTop = () => {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
 		<div className='c-layout'>
 			<Header />
 			<Outlet />
 			<Footer />
-			<div>
+			<div className='c-btn-top' onClick={handleScrollToTop}>
 				<FontAwesomeIcon icon={faChevronUp} />
 			</div>
 		</div>
