49f5bc4945446c419fd151f902726af919e00702
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-02-10 10:12:13 +0100

    add styling for 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <p>404 ERROR: Page not found.</p>
-            <Link to='/'>Back to homepage.</Link>
+        <div className='o-wrap o-wrap--flex c-notFound'>
+            <h2>404</h2>
+            <p>ooops! The page you requested was not found.</p>
+            <div className='c-btn'>
+                <Link to='/'>Back to homepage</Link>
+            </div>
         </div>
     );
 }
