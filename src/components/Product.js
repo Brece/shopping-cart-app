58c88809cd2d63ddb4fedd397fa34f90b840ab2d
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-02-14 01:58:16 +0100

    adds basic product component

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = (props) => {
+    return (
+        <section className='c-product'>
+            product xxx
+        </section>
+    );
+}
+
+export default Product;
