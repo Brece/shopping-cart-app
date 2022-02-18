989ab6f8ad8f3b8cfd6e49c5154654a521f6c1c9
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-02-18 18:09:04 +0100

    commit for the last working state, preparation for experimental branch

--- src/components/ShopView.js
+++ src/components/ShopView.js
@@ -3,6 +3,9 @@ import Breadcrumb from './Breadcrumb';
 import { Outlet } from 'react-router-dom';
 
 const ShopView = () => {
+    // TODO: 
+    // view keeps track of selected product to display
+    // refactor child components for new router structure
     const [selectedProductView, setSelectedProductView] = useState({});
 
     const handleSelectedProductView = (product) => {
