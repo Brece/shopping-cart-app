4e40d190a8abb8bcaa6b49c4a8145f91f6e1a1ab
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-02-22 13:48:07 +0100

    adds product view structure and styling, adds product tabs and review form

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShield, faArrowsRotate, faCircleDollarToSlot, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Product = () => {
-    // FIXME: remove params when not needed
-    // useParams hook to get current URL represented by path=":product"
-    const { product } = useParams();
-    
     // useLocation hook to get the passed down state from <Link /> property in ProductCard component to get the product information
     const location = useLocation();
 
     // stores product information in state
     const [state, setState] = useState(location.state);
     const [amount, setAmount] = useState(1);
+    const [tabDisplayed, setTabDisplayed] = useState('description');
 
     const handleIncreasingAmount = () => {
         setAmount(amount + 1);
@@ -51,12 +50,24 @@ const Product = () => {
             return;
         }
 
-        // initiliaze localstorage as an Array of objects when
+        // initiliaze localstorage as an Array of objects when cart doesn't exist
         localStorage.setItem('cart', JSON.stringify([{...state, amount}]));
     }
 
-    // FIXME: remove console.log
-    console.log(location.state);
+    const handleCapitalizedDescription = () => {
+        const description = state.description;
+        const firstLetter = description[0].toUpperCase();
+        const newDescription = firstLetter + description.slice(1);
+        setState({...state, description: newDescription});
+    }
+
+    const handleTabDisplayed = (val) => {
+        setTabDisplayed(val);
+    }
+
+    useEffect(() => {
+        handleCapitalizedDescription();
+    }, []);
 
     return (
         <section className='o-wrap c-product'>
@@ -68,23 +79,127 @@ const Product = () => {
                     <h3 className='c-product__info__title'>{state.title}</h3>
                     <p className='c-product__info__price'>{state.price}€</p>
                     <p className='c-product__info__teaser'>{state.description}</p>
-                    <div className='c-product__info__text__icons'>
-                        <p>icons for delivery & services xxx</p>
+                    <ul className='c-product__info__text__icons'>
+                        <li>
+                            <FontAwesomeIcon icon={faShield} />
+                            <p>1 Year MyOnlineStore Warranty</p>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faArrowsRotate} />
+                            <p>30 Day Return Policy</p>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faCircleDollarToSlot} />
+                            <p>Cash on Delivery available</p>
+                        </li>
+                    </ul>
+                    <div className='c-product__info__text__rating'>
+                        <div className='c-product__info__text__rating__stars' style={{'--rating': state.rating.rate}} />
+                        <span>{state.rating.rate} ({state.rating.count})</span>
                     </div>
-                    <p className='c-product__info__text__rating'>{state.rating.rate} ({state.rating.count})</p>
                     <div className='c-product__info__text__cart'>
                         <div className='c-product__info__text__cart__amount' onClick={handleDecreasingAmount}>-</div>
                         <input type='text' value={amount} placeholder='1' onChange={handleChange} />
                         <div className='c-product__info__text__cart__amount' onClick={handleIncreasingAmount}>+</div>
                         <button type='button' className='c-btn c-product__info__text__cart__btn' onClick={handleAddToCart}>add to cart</button>
                     </div>
-                    <p className='c-product__info__text__category'>{state.category}</p>
+                    <p className='c-product__info__text__category'>Category: {state.category}</p>
                 </div>
             </div>
-            <div className='c-product__description'>
-                <p>{state.description}</p>
-            </div>
+            <ProductTabs
+                description={state.description}
+                handleTabDisplayed={handleTabDisplayed}
+                tabDisplayed={tabDisplayed} 
+            />
         </section>
+    );
+}
+
+const ReviewForm = () => {
+    const [stars, setStars] = useState(1);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Thank You for your review.');
+    }
+
+    const handleStarRating = (e) => {
+        const rating = e.target.dataset.rating;
+        document.querySelectorAll('.c-product__tabs__form__rating__star').forEach((item, index) => {
+            if (index < rating) {
+                item.classList.add('isActive');
+                return;
+            }
+
+            item.classList.remove('isActive');
+        });
+
+        setStars(rating);
+    }
+
+    return (
+        <form className='c-product__tabs__form' onSubmit={ (e) => handleSubmit(e) }>
+            <h4>Add a review</h4>
+            <div className='c-product__tabs__form__rating'>
+                <label htmlFor='rating-1' className='c-product__tabs__form__rating__star isActive'>
+                    <FontAwesomeIcon icon={faStar} />
+                </label>
+                <input type='radio' name='rating' id='rating-1' data-rating='1' onClick={handleStarRating} selected />
+                <label htmlFor='rating-2' className='c-product__tabs__form__rating__star'>
+                    <FontAwesomeIcon icon={faStar} />
+                </label>
+                <input type='radio' name='rating' id='rating-2' data-rating='2' onClick={handleStarRating} />
+                <label htmlFor='rating-3' className='c-product__tabs__form__rating__star'>
+                    <FontAwesomeIcon icon={faStar} />
+                </label>
+                <input type='radio' name='rating' id='rating-3' data-rating='3' onClick={handleStarRating} />
+                <label htmlFor='rating-4' className='c-product__tabs__form__rating__star'>
+                    <FontAwesomeIcon icon={faStar} />
+                </label>
+                <input type='radio' name='rating' id='rating-4' data-rating='4' onClick={handleStarRating} />
+                <label htmlFor='rating-5' className='c-product__tabs__form__rating__star'>
+                    <FontAwesomeIcon icon={faStar} />
+                </label>
+                <input type='radio' name='rating' id='rating-5' data-rating='5' onClick={handleStarRating} />
+            </div>
+            <textarea rows='5' placeholder='Your review *' required />
+            <input type='text' placeholder='Enter Name *' required />
+            <input type='email' placeholder='Enter Email *' required />
+            <button type='submit' className='c-btn'>Submit Review</button>
+        </form>
+    )
+}
+
+const ProductTabs = (props) => {
+    const Description = () => {
+        return (
+            <p>{props.description}</p>
+        );
+    }
+
+    const handleClick = (e) => {
+        // change active tab
+        const tab = e.target.dataset.tab;
+        props.handleTabDisplayed(tab);
+
+        // toggle highlighted active tab
+        document.querySelectorAll('.c-product__tabs__list__item').forEach((item) => {
+            if (item.dataset.tab === tab) {
+                item.classList.add('isActive');
+                return;
+            }
+            item.classList.remove('isActive');
+        });
+    }
+
+    return (
+        <div className='c-product__tabs'>
+            <ul className='c-product__tabs__list'>
+                <li className='c-product__tabs__list__item isActive' onClick={handleClick} data-tab='description'>description</li>
+                <li className='c-product__tabs__list__item' onClick={handleClick} data-tab='review'>reviews</li>
+            </ul>
+            { props.tabDisplayed === 'review' ? <ReviewForm /> : <Description />}
+        </div>
     );
 }
 
